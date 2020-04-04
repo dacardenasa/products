e63108e7e04b86a357b6ce62d0a3e69aa4968149
dacardenasa <diego.cardenasaleg@gmail.com>
2020-04-04 12:15:17 -0500

    Add form_submit function to helper products

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,8 @@
 module ProductsHelper
   def form_title 
-    @product.new_record? ? "Crear Producto" : "Editar Producto"
+    @product.new_record? ? "Nuevo Producto" : "Editar Producto"
+  end
+  def form_submit
+    @product.new_record? ? "Crear Producto" : "Actualizar Producto"
   end
 end
